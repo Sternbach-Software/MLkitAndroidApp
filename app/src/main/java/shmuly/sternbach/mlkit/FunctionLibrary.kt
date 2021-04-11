cd251140634dd5177cbd64d1ce97f79bd09b0512
shmuel <shmuelsternbach613@gmail.com>
2021-04-11 19:22:25 -0400

    Initial commit

---
+++ app/src/main/java/shmuly/sternbach/mlkit/FunctionLibrary.kt
@@ -0,0 +1,27 @@
+package shmuly.sternbach.mlkit
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+
+class FunctionLibrary {
+    /*fun loadData(inFile: Int): StringBuilder {
+        var tContents: String? = ""
+
+        val stringBuilder = StringBuilder()
+        val isa: InputStream = mEntireApplicationContext.resources.openRawResource(inFile)
+        val reader = BufferedReader(InputStreamReader(isa))
+        while (true) {
+            try {
+                tContents = reader.readLine()
+                if (tContents == null) break
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            stringBuilder.appendLine(tContents)
+        }
+//        Timber.v("File Contents: $stringBuilder")
+        return stringBuilder
+    }*/
+}
